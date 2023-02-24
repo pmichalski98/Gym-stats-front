51827da41301d65122edacf15d06ea4b3f577702
piotr <piotrek.priv.michalski@wp.pl>
2023-02-24 19:46:43 +0100

    minor changes

--- src/components/MainPage/AllTrainingsShow.jsx
+++ src/components/MainPage/AllTrainingsShow.jsx
@@ -1,22 +1,21 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useState} from "react";
 import TrainingShow from "./TrainingShow.jsx";
 import Button from "../../utilcomponents/Button.jsx";
 
 function AllTrainingsShow({trainings}) {
-
     const [chosen, setChosen] = useState(false);
-    const [chosenTraining, setChosenTraining] = useState(undefined)
+    const [chosenTraining, setChosenTraining] = useState(undefined);
 
     let content;
-    let afterChoosing = (
+  let afterChoosing = (
         <div>
             <TrainingShow training={chosenTraining} setChosen={setChosen}/>
         </div>
-    );
+  );
 
     function handleChoice(training) {
-        setChosen(true)
-        setChosenTraining(training)
+        setChosen(true);
+        setChosenTraining(training);
     }
 
     let renderedTrainings = trainings.map((training, index) => {
@@ -30,30 +29,24 @@ function AllTrainingsShow({trainings}) {
                 {training.name}
             </Button>
         );
-    })
+    });
 
     let beforeChoosing = (
         <div className="w-10/12 mx-auto text-center mt-6">
             <h1 className="text-4xl">Wybierz trening</h1>
-            <div className="flex justify-around mt-10">
-                {renderedTrainings}
-            </div>
+            <div className="flex justify-around mt-10">{renderedTrainings}</div>
         </div>
-    )
+    );
 
     if (chosen === false) {
-        content = beforeChoosing
+        content = beforeChoosing;
     } else {
         content = afterChoosing;
     }
 
     // @TODO zrobic reusable input i dokonczyc stylowanie main page
 
-    return (
-        <Fragment>
-            {content}
-        </Fragment>
-    );
+    return <Fragment>{content}</Fragment>;
 }
 
 export default AllTrainingsShow;
--- src/components/MainPage/TrainingShow.jsx
+++ src/components/MainPage/TrainingShow.jsx
@@ -4,7 +4,6 @@ import {useUpdateTrainingsMutation} from "../../store/index.js";
 import Button from "../../utilcomponents/Button.jsx";
 
 function TrainingShow({training, setChosen}) {
-
     const [data, setData] = useState(training);
     const [editRow, setEditRow] = useState(null);
     const [formData, setFormData] = useState({});
@@ -12,7 +11,7 @@ function TrainingShow({training, setChosen}) {
     const [updateTrainings] = useUpdateTrainingsMutation();
 
     function handleSavingTraining() {
-        updateTrainings(data)
+        updateTrainings(data);
     }
 
     function handleEditFormChange(e) {
@@ -22,17 +21,17 @@ function TrainingShow({training, setChosen}) {
         const fieldValue = e.target.value;
 
         const updatedData = {...formData};
-        if (fieldName === 'name') {
+        if (fieldName === "name") {
             updatedData[fieldName] = fieldValue;
         } else {
             updatedData[fieldName] = parseInt(fieldValue);
         }
-        setFormData(updatedData)
+        setFormData(updatedData);
     }
 
     function handleEdit(event, exercise, index) {
         event.preventDefault();
-        setEditRow(index)
+        setEditRow(index);
         const defaultValues = {
             id: exercise.id,
             name: exercise.name,
@@ -46,67 +45,120 @@ function TrainingShow({training, setChosen}) {
     function handleSave(event) {
         event.preventDefault();
 
-        const updatedExercises = data.exercises
-            .map(exercise => exercise.id === formData.id
-                ? {...exercise, ...formData} : {...exercise})
+        const updatedExercises = data.exercises.map((exercise) =>
+            exercise.id === formData.id
+                ? {...exercise, ...formData}
+                : {...exercise}
+        );
 
         const dataAfterEdit = {
             ...data,
-            exercises: updatedExercises
-        }
+            exercises: updatedExercises,
+        };
 
-        setData(dataAfterEdit)
-        setEditRow(null)
+        setData(dataAfterEdit);
+        setEditRow(null);
     }
 
     const config = [
         {
-            label: '',
-            render: (exercise, index) => index + 1
+            label: "",
+            render: (exercise, index) => index + 1,
         },
         {
-            label: 'Ćwiczenie',
-            render: (exercise, index) => editRow === index ?
-                <input type="text" name="name" value={formData.name}
-                       onChange={(e) => handleEditFormChange(e)}/> : exercise.name
-        },
-        {
-            label: 'Serie',
-            render: (exercise, index) => editRow === index ?
-                <input type="number" name="sets" value={formData.sets}
-                       onChange={handleEditFormChange}/> : exercise.sets
-        },
-        {
-            label: 'Powtorzenia',
-            render: (exercise, index) => editRow === index ?
-                <input className="focus:outline-amber-300 w-full text-center" type="number" name="reps" value={formData.reps}
-                       onChange={handleEditFormChange}/> : exercise.reps
-        },
-        {
-            label: 'Ciężar',
-            render: (exercise, index) => editRow === index ?
-                <input type="number" name="weight" value={formData.weight}
-                       onChange={handleEditFormChange}/> : exercise.weight + ' kg'
-        },
-        {
-            label: 'Actions',
+            label: "Ćwiczenie",
             render: (exercise, index) =>
-                index !== editRow ?
-                    <Button success pad rounded key={index} onClick={(event) => handleEdit(event, exercise, index)}
-                            >Edit</Button> :
-                    <Button success pad rounded key={index} type="submit" >Save</Button>
-        }
-    ]
+                editRow === index ? (
+                    <input
+                        type="text"
+                        name="name"
+                        value={formData.name}
+                        onChange={(e) => handleEditFormChange(e)}
+                    />
+                ) : (
+                    exercise.name
+                ),
+        },
+        {
+            label: "Serie",
+            render: (exercise, index) =>
+                editRow === index ? (
+                    <input
+                        type="number"
+                        name="sets"
+                        value={formData.sets}
+                        onChange={handleEditFormChange}
+                    />
+                ) : (
+                    exercise.sets
+                ),
+        },
+        {
+            label: "Powtorzenia",
+            render: (exercise, index) =>
+                editRow === index ? (
+                    <input
+                        className="focus:outline-amber-300 w-full text-center"
+                        type="number"
+                        name="reps"
+                        value={formData.reps}
+                        onChange={handleEditFormChange}
+                    />
+                ) : (
+                    exercise.reps
+                ),
+        },
+        {
+            label: "Ciężar",
+            render: (exercise, index) =>
+                editRow === index ? (
+                    <input
+                        type="number"
+                        name="weight"
+                        value={formData.weight}
+                        onChange={handleEditFormChange}
+                    />
+                ) : (
+                    exercise.weight + " kg"
+                ),
+        },
+        {
+            label: "Actions",
+            render: (exercise, index) =>
+                index !== editRow ? (
+                    <Button
+                        success
+                        pad
+                        rounded
+                        key={index}
+                        onClick={(event) => handleEdit(event, exercise, index)}
+                    >
+                        Edit
+                    </Button>
+                ) : (
+                    <Button success pad rounded key={index} type="submit">
+                        Save
+                    </Button>
+                ),
+        },
+    ];
 
     return (
-        <div className=" w-10/12 container mx-auto content-center grid gap-4">
+        <div className=" w-10/12 container mx-auto content-center grid gap-4 ">
             <h1 className="text-center p-4 text-5xl">{training.name}</h1>
-            <form className="text-center grid inline-grid grid-cols-6 gap-4 mx-auto container max-w-3xl" onSubmit={handleSave}>
+            <form
+                className="text-center grid inline-grid grid-cols-6 gap-4 mx-auto container max-w-3xl"
+                onSubmit={handleSave}
+            >
                 <TrainingTable config={config} data={data.exercises}/>
             </form>
             <div className="flex justify-around ">
-                <Button primary pad rounded onClick={handleSavingTraining}>Save training</Button>
-                <Button secondary roundedFull pad onClick={() => setChosen(false)}>Go back</Button>
+                <Button primary pad rounded onClick={handleSavingTraining}>
+                    Save training
+                </Button>
+                <Button secondary roundedFull pad onClick={() => setChosen(false)}>
+                    Go back
+                </Button>
             </div>
         </div>
     );
